2447af2800e2f1a05f79cecc1b90ade13518b8c9
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2023-11-23 16:26:54 -0400

    findbyemail user

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,4 +1,29 @@
-export default class TopicService {
+export default class UserService {
+
+    async findByEmailContainingIgnoreCase(text) { //TODO: falta token
+        const url = `http://localhost:8081/api/v1/users/findByEmail?email=${text}`;
+        const options = {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                // Authorization: token,
+            }
+        };
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error(`HTTP error: Status: ${response.status}`);
+            }
+            const users = await response.json();
+            console.log(`se obtuvo los resultados de buscar ${text} en UserService `+users.data);
+            console.log("obtuvo usuarios con acceso"+JSON.stringify(users.data));
+
+            return users.data;
+        } catch (error) {
+            console.error('Error al obtener los usuarios con acceso:', error);
+        }
+    }
+    
     async getUserProfile(email) { //TODO: falta token
         const url = `http://localhost:8081/api/v1/users/profile?email=${email}`; //?shared=${shared}&?favorite=${favorite}
         const options = {
