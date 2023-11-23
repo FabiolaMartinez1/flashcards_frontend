a7f8d7895c2b07a8fef0c43c48bdeecc28918a73
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2023-11-23 16:31:59 -0400

    ger profile devuelta a userservice

--- src/service/UserService.js
+++ src/service/UserService.js
@@ -1,5 +1,28 @@
 export default class UserService {
 
+    async getUserProfile(email) { //TODO: falta token
+        const url = `http://localhost:8081/api/v1/users/profile?email=${email}`; //?shared=${shared}&?favorite=${favorite}
+        const options = {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                // Authorization: token,
+            }
+        };
+        try {
+            const response = await fetch(url, options);
+            if (!response.ok) {
+                throw new Error(`HTTP error: Status: ${response.status}`);
+            }
+            const profile = await response.json();
+            console.log("perfil en service "+profile.data);
+            console.log("obtuvo perfil"+JSON.stringify(profile.data));
+            return profile.data;
+        } catch (error) {
+            console.error('Error al obtener el perfil:', error);
+        }
+    }
+    
     async findByEmailContainingIgnoreCase(text) { //TODO: falta token
         const url = `http://localhost:8081/api/v1/users/findByEmail?email=${text}`;
         const options = {
