1917431bfd46f27dec849c9a830b9d84fac81e47
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2023-11-30 08:17:50 -0400

    front crud tags

--- src/components/TagManagement.vue
+++ src/components/TagManagement.vue
@@ -7,18 +7,50 @@
       <i class="bi bi-filter"></i> Etiquetas
     </button>
     <ul class="dropdown-menu p-2" style="width: auto; padding: 1rem" @click.stop>
-      <h5>Etiquetas <i class="bi bi-pencil-square fs-5"></i></h5>
+      <h5>Etiquetas <i v-if="!flagEditableMode" class="tag bi bi-pencil-square fs-4" @click="editableMode"></i></h5>
       <hr>
-      <div class="d-flex flex-wrap">
+      <div v-if="!flagEditableMode" class="d-flex flex-wrap">
         <li v-for="(etiqueta, index) in etiquetas" :key="index" class="me-2 mb-2">
           <input type="checkbox" :id="`tag-${index}`" v-model="selectedTags" :value="etiqueta.name" @change="sendData"/>
           <label :for="`tag-${index}`">{{ etiqueta.name }}</label>
         </li>
       </div>
+
+      <div v-if="flagEditableMode" class="d-flex flex-wrap">
+        <li v-for="(etiqueta, index) in etiquetas" :key="index" class="d-flex align-items-center">
+          <input class="form-control form-control editable" :value="etiqueta.name" @keyup.enter="updateTag(index, $event.target.value)" />
+          <i class="fas fa-pen icon edit-icon" @click="enableEditing(index)"></i>
+          <i class="fas fa-trash icon delete-icon" @click="deleteTag(index)"></i>
+        </li>
+        <li class="d-flex align-items-center">
+          <input class="form-control form-control editable" 
+            v-model="newTagName"
+            @keyup.enter="addTag"
+            @blur="isAddingTag = false"
+          />
+          <!-- icono add -->
+          <i class="fas fa-plus-circle  icon" @click="addTag"></i>
+          
+        </li>
+<!--         
+          
+
+        <div class="d-flex align-items-center me-2 mb-2">
+          <input 
+            v-if="isAddingTag"
+            class="form-control form-control-lg editable" 
+            v-model="newTagName"
+            @keyup.enter="addTag"
+            @blur="isAddingTag = false"
+          />
+          <button v-else class="btn btn-primary" @click="isAddingTag = true">+ Nueva etiqueta</button>
+        </div> -->
+      </div>
+
       <hr>
       <div class="d-flex justify-content-center">
-        <button class="btn btn-outline-secondary me-4" @click="cerrarDropdown" style="color: white; background-color: #4F2A93; border-color: #4F2A93">Cancelar</button>
-        <button class="btn btn-outline-secondary ms-4" @click="cerrarDropdown" style="color: white; background-color: #4F2A93; border-color: #4F2A93">Aceptar</button>
+        <button v-if="!flagEditableMode" class="btn btn-outline-secondary me-4" @click="cerrarDropdown" style="color: white; background-color: #4F2A93; border-color: #4F2A93">Cancelar</button>
+        <button v-if="flagEditableMode" class="btn btn-outline-secondary ms-4" @click="cerrarDropdown" style="color: white; background-color: #4F2A93; border-color: #4F2A93">Aceptar</button>
       </div>
     </ul>
   </div>
@@ -35,6 +67,7 @@ export default{
       token: null,
       sub: null,
       user: null,
+      flagEditableMode: false,
     }
   },
   created() {
@@ -65,10 +98,36 @@ export default{
       }
 
     },
+    addTag() {
+      if (this.newTagName.trim()) {
+        this.etiquetas.push({ name: this.newTagName });
+        this.newTagName = '';
+        this.isAddingTag = false;
+        // Aquí puedes incluir lógica adicional, como enviar la nueva etiqueta al servidor
+      }
+    },
+    deleteTag(index) {
+      this.etiquetas.splice(index, 1);
+      // Aquí puedes incluir lógica adicional, como eliminar la etiqueta del servidor
+    },
+    enableEditing(index) {
+      this.$nextTick(() => {
+        let input = document.getElementsByClassName('editable')[index];
+        input.focus();
+      });
+    },
+    updateTag(index, value) {
+      this.etiquetas[index].name = value;
+      // Aquí puedes incluir lógica adicional, como actualizar la etiqueta en el servidor
+    },
     cerrarDropdown(event) {
       event.stopPropagation();
       let dropdown = document.getElementById('dropdownMenuButton');
       dropdown.click();
+      this.flagEditableMode = false;
+    },
+    editableMode() {
+      this.flagEditableMode = !this.flagEditableMode;
     }
   }
 }
@@ -89,6 +148,7 @@ export default{
 
 .d-flex.flex-wrap li {
   width: calc(50% - 0.5rem); /* Ajusta el ancho para dos columnas */
+  max-width: 50%; /* Ancho máximo */
 }
 
 /* Estilo para los botones de cerrar y cancelar */
@@ -101,4 +161,42 @@ export default{
   margin-right: 10px; 
   /* Espaciado entre el checkbox y la etiqueta */
 }
+.tag {
+    font-size: 1.5rem; /* Tamaño de los íconos */
+    color: #36205D; /* Color de los íconos */
+    font-weight: bold; /* icono grueso? */
+  }
+/*  */
+.editable {
+  width: calc(30% - 0.5rem);
+  /* flex-grow: 0.5rem!important; Hace que el input ocupe el espacio restante */
+  margin-right: 0.5rem; /*Espacio a la derecha del input*/
+  /* margin-right: 0.5rem!important; */
+  border: none;
+  border-bottom: 1px solid #ced4da; /* Línea inferior para el input */
+  border-radius: 0; /* Sin bordes redondeados */
+  box-shadow: none; /* Sin sombra */
+  padding-left: 0; /* Sin padding izquierdo */
+  padding-right: 0; /* Sin padding derecho */
+  outline: none; /* Remueve el borde de foco por defecto */
+  height: calc(1.5em + 0.75rem + 2px); /* Altura alineada con botones y etiquetas */
+}
+
+.editable:focus {
+  border-bottom: 2px solid #4F2A93; /* Línea inferior más gruesa al enfocar */
+}
+
+/* Estilo para los íconos de editar y eliminar */
+.icon {
+  font-size: 1rem; /* Tamaño de los íconos */
+  color: #4F2A93; /* Color de los íconos */
+  cursor: pointer;
+  margin-left: 0.5rem; /* Espaciado a la izquierda de los íconos */
+}
+
+/* Ajuste del tamaño de los botones */
+.btn {
+  padding: 0.375rem 0.75rem;
+}
+
 </style>
