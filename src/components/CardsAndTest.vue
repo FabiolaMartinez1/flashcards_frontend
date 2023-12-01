ed4fc34421ff798510c93b2d3283f13b3d2dfe58
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2023-12-01 10:50:32 -0400

    getFlashcards

--- src/components/CardsAndTest.vue
+++ src/components/CardsAndTest.vue
@@ -16,28 +16,29 @@
   <p class="d-flex">Descripción... de mi tema...</p>
   <div class="card">
         <div class="card-body">
-          <div class="overflow-auto" style="max-height: 400px;">
+          <div class="overflow-auto" style="height: 450px;">
           <!-- Bucle v-for para mostrar las flashcards -->
           <div class="card mb-3" v-for="(flashcard, index) in flashcards" :key="index">
             <div class="card-body">
               <div class="d-flex justify-content-between align-items-center mb-2">
                 <div class="d-flex align-items-center">
-                  <i class="fas bi bi-lightbulb-fill fa-2x me-2 icon-yellow"></i>
-                  <i class="fas bi bi-person-fill fa-2x me-2 icon-blue"></i>
-                  <h5 class="card-title mb-0">{{ flashcard.title }}</h5>
+                  <i v-if="flashcard.ai == 1" class="fas bi bi-lightbulb-fill fa-2x me-2 icon-yellow"></i>
+                  <i v-else class="fas bi bi-person-fill fa-2x me-2 icon-blue"></i>
+                  <h5 class="card-title">Card {{index+1}}: {{ flashcard.front.substring(0, 45) + '...'  }}</h5>
+
                 </div>
                 <div class="dropdown">
                   <button class="btn btn-sm btn-transparent" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="bi bi-three-dots-vertical fs-5"></i>
                   </button>
                   <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                    <li><a class="dropdown-item" href="#">Editar</a></li>
+                    <li><a v-if="flashcard.ai != 1" class="dropdown-item" href="#">Editar</a></li>
                     <li><a class="dropdown-item" href="#">Eliminar</a></li>
                     <li><a class="dropdown-item" href="#">Ver detalles</a></li>
                   </ul>
                 </div>
               </div>
-              <p class="d-flex card-text">{{ flashcard.description }}</p>
+              <p class="d-flex card-text">{{ flashcard.back.substring(0, 90) + '...'  }}</p>
             </div>
           </div>
         </div>
@@ -47,7 +48,7 @@
   
       <!-- Score Section Column -->
       <div class="col-md-4">
-        <div style="height: 100px;"> <!-- Ajusta el alto según sea necesario -->
+        <div style="height: 94px;"> <!-- Ajusta el alto según sea necesario -->
           <!-- Espacio vacío -->
         </div>
         <div class="card text-center">
@@ -55,7 +56,9 @@
             Modo test
           </div>
           <div class="card-body" style="height: 440px;">
-            <h5 class="card-title">Puntuación:</h5>
+            <br>
+            <h4 class="card-title">Puntuación:</h4>
+            
             <div class="progress-circle">
         <svg viewBox="0 0 36 36" class="circular-chart">
           <path class="circle-bg"
@@ -83,8 +86,14 @@
       
         </svg>
       </div>
+      <br>
       <div class="total">11/15</div>  
-          </div>
+      <br>
+      <h4>Correctas: #</h4>
+      <h4>Incorrectas: #</h4>
+      <span>Último vez: dd/mm/aaaa</span>
+      </div>
+      
         </div>
       </div>
     </div>
@@ -92,18 +101,46 @@
   
   </template>
 <script>
+import FlashcardsService from '../service/FlashcardService';
 export default {
   data() {
     return {
       // Datos de ejemplo para las flashcards
-      flashcards: [
-        { title: 'Card#: Nombre card 1', description: 'Descripción aquí 1' },
-        { title: 'Card#: Nombre card 2', description: 'Descripción aquí 2' },
-        // ... más flashcards
-      ]
+      // flashcards: [
+      //   { title: 'Card#: Nombre card 1', description: 'Descripción aquí 1' },
+      //   { title: 'Card#: Nombre card 2', description: 'Descripción aquí 2' },
+      //   // ... más flashcards
+      // ]
+      flashcards: [],
+      user: null,
+      sub: null,
+      topicId: null,
     };
   },
-  // Métodos para editar, eliminar, ver detalles, etc.
+  created() {
+    this.flashcardsService = new FlashcardsService();
+    this.user = this.$auth0.user;
+    this.sub = this.user.sub;
+    console.log("sub en topicCard: "+this.sub);
+    this.getFlashcards();
+  },
+  methods: {
+    async getFlashcards() {
+      try{
+        this.user =  this.$auth0.user;
+        this.sub =  this.user.sub;
+        console.log("sub: "+this.sub);
+        const data = await this.flashcardsService.getFlashcards(3,this.sub);
+        //TODO: validar el estado de la respuesta
+        console.log("getFlashcards: "+data.responseCode);
+        console.log("getFlashcards: "+data.data);
+        this.flashcards = data.data;
+        console.log("en topicCard"+this.flashcards);
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
 <style>
@@ -116,7 +153,7 @@ export default {
 .circular-chart {
   display: block;
   margin: 10px auto;
-  max-width: 40%;
+  max-width: 35%;
   max-height: 250px;
 }
 
