89ea1d659ecde6e2926606f4c358c7f41434fe15
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2023-12-01 10:11:03 -0400

    viewCards

--- src/components/CardsAndTest.vue
+++ src/components/CardsAndTest.vue
@@ -14,44 +14,76 @@
   </div>
   
   <p class="d-flex">Descripción... de mi tema...</p>
-      <div class="card">
+  <div class="card">
         <div class="card-body">
-          <!-- Contenedor para título y botón de menú -->
-          <div class="d-flex justify-content-between align-items-center mb-2">
-            <h5 class="card-title">vamoh</h5>
-            <div class="dropdown">
-              <button class="btn btn-sm btn-transparent" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
-                <i class="bi bi-three-dots-vertical fs-5"></i>
-              </button>
-              <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
-                <li><a class="dropdown-item" href="#" @click.prevent="deleteTopic(tema.topicId)">Eliminar</a></li>
-                <li><a class="dropdown-item" href="#" @click.prevent="ViewCards(tema.topicId)">Ver detalles</a></li>
-                <li><a class="dropdown-item" href="#" @click.prevent="compartir(tema.topicId, tema.title)" >Compartir</a></li>
-              </ul>
+          <div class="overflow-auto" style="max-height: 400px;">
+          <!-- Bucle v-for para mostrar las flashcards -->
+          <div class="card mb-3" v-for="(flashcard, index) in flashcards" :key="index">
+            <div class="card-body">
+              <div class="d-flex justify-content-between align-items-center mb-2">
+                <div class="d-flex align-items-center">
+                  <i class="fas bi bi-lightbulb-fill fa-2x me-2 icon-yellow"></i>
+                  <i class="fas bi bi-person-fill fa-2x me-2 icon-blue"></i>
+                  <h5 class="card-title mb-0">{{ flashcard.title }}</h5>
+                </div>
+                <div class="dropdown">
+                  <button class="btn btn-sm btn-transparent" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="bi bi-three-dots-vertical fs-5"></i>
+                  </button>
+                  <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="dropdownMenuButton">
+                    <li><a class="dropdown-item" href="#">Editar</a></li>
+                    <li><a class="dropdown-item" href="#">Eliminar</a></li>
+                    <li><a class="dropdown-item" href="#">Ver detalles</a></li>
+                  </ul>
+                </div>
+              </div>
+              <p class="d-flex card-text">{{ flashcard.description }}</p>
             </div>
           </div>
         </div>
-      </div>
     </div>
+  </div>
+  </div>
   
       <!-- Score Section Column -->
       <div class="col-md-4">
-        <div style="height: 100px;"> <!-- Ajusta el ancho según sea necesario -->
+        <div style="height: 100px;"> <!-- Ajusta el alto según sea necesario -->
           <!-- Espacio vacío -->
         </div>
         <div class="card text-center">
           <div class="card-header">
             Modo test
           </div>
-          <div class="card-body">
+          <div class="card-body" style="height: 440px;">
             <h5 class="card-title">Puntuación:</h5>
-            <p class="card-text">
-              <!-- Use Bootstrap Progress bar or custom circle progress bar here -->
-              <!-- <div class="progress">
-                <div class="progress-bar" role="progressbar" style="width: 70%;" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">70</div>
-              </div> -->
-              11/15
-            </p>
+            <div class="progress-circle">
+        <svg viewBox="0 0 36 36" class="circular-chart">
+          <path class="circle-bg"
+            d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"
+            fill="none"
+            stroke="#eee"
+            stroke-width="2.8"
+            stroke-linecap="round"
+          />
+
+          <!-- El atributo 'stroke-dasharray' determina la longitud de la barra de progreso -->
+          <path class="circle"
+            d="M18 2.0845
+              a 15.9155 15.9155 0 0 1 0 31.831
+              a 15.9155 15.9155 0 0 1 0 -31.831"
+            fill="none"
+            stroke="#000"
+            stroke-width="2.8"
+            stroke-linecap="round"
+            stroke-dasharray="70, 100"
+          />
+          <text x="18" y="20.35" class="score" text-anchor="middle" alignment-baseline="central" font-size="4" fill="#333">70</text>
+      
+        </svg>
+      </div>
+      <div class="total">11/15</div>  
           </div>
         </div>
       </div>
@@ -59,6 +91,53 @@
   </div>  
   
   </template>
-  <script>
-  
-  </script>
+<script>
+export default {
+  data() {
+    return {
+      // Datos de ejemplo para las flashcards
+      flashcards: [
+        { title: 'Card#: Nombre card 1', description: 'Descripción aquí 1' },
+        { title: 'Card#: Nombre card 2', description: 'Descripción aquí 2' },
+        // ... más flashcards
+      ]
+    };
+  },
+  // Métodos para editar, eliminar, ver detalles, etc.
+};
+</script>
+<style>
+.icon-yellow {
+  color: #ffc107; /* Color amarillo de Bootstrap para íconos */
+}
+.icon-blue{
+  color: #4695BD /*#2A5AA3; /* Color azul de Bootstrap para íconos */
+}
+.circular-chart {
+  display: block;
+  margin: 10px auto;
+  max-width: 40%;
+  max-height: 250px;
+}
+
+.circle-bg {
+  fill: none;
+  stroke: #eee;
+}
+
+.circle {
+  fill: none;
+  stroke-width: 2.8;
+  stroke-linecap: round; /* para bordes redondeados en el camino */
+}
+
+.score {
+  font-size: 0.54em;
+  color: #000;
+}
+
+.total {
+  font-size: 1.2em;
+  color: #666;
+}
+</style>
