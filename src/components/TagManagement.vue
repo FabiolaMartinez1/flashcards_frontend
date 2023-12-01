354d2317a3c7271176b57eb787d076d7dbecbd5e
Fabi Martinez <104600297+FabiolaMartinez1@users.noreply.github.com>
2023-11-30 23:16:06 -0400

    front crud tag funcional

--- src/components/TagManagement.vue
+++ src/components/TagManagement.vue
@@ -6,17 +6,21 @@
       style="color: white; background-color: #4F2A93; border-color: #4F2A93">
       <i class="bi bi-filter"></i> Etiquetas
     </button>
-    <ul class="dropdown-menu p-2" style="width: auto; padding: 1rem" @click.stop>
-      <h5>Etiquetas <i v-if="!flagEditableMode" class="tag bi bi-pencil-square fs-4" @click="editableMode"></i></h5>
+    <ul class="dropdown-menu p-2 common-ul" @click.stop>
+      <h5>Etiquetas 
+        <i v-if="!flagEditableMode" class="tag bi bi-pencil-square fs-4" @click="editableMode">
+        </i>
+        <i v-else class="tag bi bi-check2-square fs-4" @click="editableMode"></i>
+      </h5>
       <hr>
-      <div v-if="!flagEditableMode" class="d-flex flex-wrap">
-        <li v-for="(etiqueta, index) in etiquetas" :key="index" class="me-2 mb-2">
+      <div v-if="!flagEditableMode" class="d-flex flex-wrap" style="width: auto; padding: 1rem" >
+        <li v-for="(etiqueta, index) in etiquetas" :key="index" class="me-2 mb-2 d-flex">
           <input type="checkbox" :id="`tag-${index}`" v-model="selectedTags" :value="etiqueta.name" @change="sendData"/>
           <label :for="`tag-${index}`">{{ etiqueta.name }}</label>
         </li>
       </div>
 
-      <div v-if="flagEditableMode" class="d-flex flex-wrap">
+      <div v-if="flagEditableMode" class="d-flex flex-wrap" style="width: auto; padding: 1rem" >
         <li v-for="(etiqueta, index) in etiquetas" :key="index" class="d-flex align-items-center">
           <input class="form-control form-control editable" :value="etiqueta.name" @keyup.enter="updateTag(index, $event.target.value)" />
           <i class="fas fa-pen icon edit-icon" @click="enableEditing(index)"></i>
@@ -24,27 +28,12 @@
         </li>
         <li class="d-flex align-items-center">
           <input class="form-control form-control editable" 
-            v-model="newTagName"
-            @keyup.enter="addTag"
-            @blur="isAddingTag = false"
+            v-model="newTagName" @keyup.enter="addTag" @blur="isAddingTag = false"
           />
           <!-- icono add -->
           <i class="fas fa-plus-circle  icon" @click="addTag"></i>
           
         </li>
-<!--         
-          
-
-        <div class="d-flex align-items-center me-2 mb-2">
-          <input 
-            v-if="isAddingTag"
-            class="form-control form-control-lg editable" 
-            v-model="newTagName"
-            @keyup.enter="addTag"
-            @blur="isAddingTag = false"
-          />
-          <button v-else class="btn btn-primary" @click="isAddingTag = true">+ Nueva etiqueta</button>
-        </div> -->
       </div>
 
       <hr>
@@ -68,6 +57,7 @@ export default{
       sub: null,
       user: null,
       flagEditableMode: false,
+      newTagName: '',
     }
   },
   created() {
@@ -143,7 +133,7 @@ export default{
 .d-flex.flex-wrap {
   display: flex;
   flex-wrap: wrap;
-  justify-content: flex-startt;
+  justify-content: flex-start;
 }
 
 .d-flex.flex-wrap li {
@@ -168,9 +158,9 @@ export default{
   }
 /*  */
 .editable {
-  width: calc(30% - 0.5rem);
+  width: calc(50% - 0.5rem);
   /* flex-grow: 0.5rem!important; Hace que el input ocupe el espacio restante */
-  margin-right: 0.5rem; /*Espacio a la derecha del input*/
+  /* margin-right: 0.5rem; Espacio a la derecha del input */
   /* margin-right: 0.5rem!important; */
   border: none;
   border-bottom: 1px solid #ced4da; /* Línea inferior para el input */
@@ -199,4 +189,21 @@ export default{
   padding: 0.375rem 0.75rem;
 }
 
+.li-d-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: flex-start;
+}
+.li-d{
+  width: calc(50% - 0.5rem); /* Ajusta el ancho para dos columnas */
+  max-width: 50%; /* Ancho máximo */
+}
+
+.common-ul {
+  width: calc(50% - 0.5rem); /* Ajusta el ancho descontando el padding */
+  padding: 1rem;
+}
+.label{
+  word-break: break-word;
+}
 </style>
